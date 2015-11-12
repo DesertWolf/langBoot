7c3983f279768cfc334ae85dabd58a95d8dc4aef
DesertWolf <jgfields@gmail.com>
2015-11-12 14:18:35 -0700

    added jumbotron and spanish translations

--- js/langpack/nonDynamic.js
+++ js/langpack/nonDynamic.js
@@ -3,6 +3,8 @@ Lang.prototype.pack.es = {
 	"token": {
 		"Property Search":"Buscar propiedad",
 		"Hello, world!":"¡Hola mundo!",
+		"This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.":"Esta es una plantilla para una comercialización simple o sitio web informativo . Incluye una gran llamada llamado jumbotron y tres piezas de soporte de contenido. Úselo como punto de partida para crear algo más único.",
+		"Learn more":"Aprende más",
 },
 	"regex": [
 
