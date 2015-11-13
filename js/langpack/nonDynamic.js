4d4bbebd5e4eebc01909d7eefd3ebf901ddcf9bb
DesertWolf <jgfields@gmail.com>
2015-11-13 08:29:41 -0700

    added new 3 column row, a next page and an image that changes depending on language. Also language persists between page changes.

--- js/langpack/nonDynamic.js
+++ js/langpack/nonDynamic.js
@@ -5,6 +5,10 @@ Lang.prototype.pack.es = {
 		"Hello, world!":"¡Hola mundo!",
 		"This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.":"Esta es una plantilla para una comercialización simple o sitio web informativo . Incluye una gran llamada llamado jumbotron y tres piezas de soporte de contenido. Úselo como punto de partida para crear algo más único.",
 		"Learn more":"Aprende más",
+        "Next Page":"Pagina siguiente",
+        "Next Page!":"¡Pagina siguiente!",
+        "images/enFlag.jpeg":"images/esFlag.png",
+        "US Flag":"Bandera de España"
 },
 	"regex": [
 
